47723f4a72dfeecb25db521407580db99002a7d0
Garrit <garrit.schaap@ju.se>
2022-04-15 13:01:26 +0200

    Added html example

---
+++ html/example.js
@@ -0,0 +1,11 @@
+const headlineElement = document.querySelector("h1");
+console.log(headlineElement);
+
+const footerElement = document.getElementById("footer");
+console.log(footerElement);
+
+const termElements = document.getElementsByClassName("term");
+console.log(termElements);
+
+const paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs);
