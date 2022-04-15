1b755b27a6d660458562c33803fc0a1b4d35ec82
Garrit <garrit.schaap@ju.se>
2022-04-15 13:40:03 +0200

    Added the fruit shop

---
+++ html/fruitshop/fruit.js
@@ -0,0 +1,49 @@
+const fruits = ["🍒", "🍍", "🍓", "🍎", "🍋", "🍇", "🍉", "🥝"];
+const fruitListElement = document.getElementById("fruitList");
+const shoppingCartElement = document.getElementById("shoppingCart");
+const amountElement = document.getElementById("amount");
+
+let shoppingList = [];
+
+for (let fruit of fruits) {
+  const fruitElement = document.createElement("div");
+  fruitElement.innerText = fruit;
+  fruitElement.classList.add("fruit");
+  fruitElement.onclick = addToShoppingList;
+  fruitListElement.appendChild(fruitElement);
+}
+
+function addToShoppingList() {
+  shoppingList.push(this.innerText);
+
+  const cartElement = document.createElement("div");
+
+  const spanElement = document.createElement("span");
+  spanElement.innerText = this.innerText;
+  cartElement.appendChild(spanElement);
+
+  const button = document.createElement("button");
+  button.innerText = "Remove";
+  button.onclick = removeElement;
+  cartElement.appendChild(button);
+
+  shoppingCartElement.appendChild(cartElement);
+
+  updateAmount();
+}
+
+function removeElement() {
+  const element = this.parentNode;
+
+  const fruitElement = element.querySelector("span");
+  const emoji = fruitElement.innerText;
+  const emojiIndex = shoppingList.indexOf(emoji);
+  shoppingList.splice(emojiIndex, 1);
+
+  element.parentNode.removeChild(element);
+  updateAmount();
+}
+
+function updateAmount() {
+  amountElement.innerText = "Items: " + shoppingList.length;
+}
